340f26720719b15c864bb442a990cc8bccf85fb0
Mustafa Dikyar <mustafadikyar1@gmail.com>
2021-01-05 15:16:15 +0300

    Redis implementation

--- Msdi.Core/CrossCuttingConcerns/Caching/Redis/RedisCacheManager.cs
+++ Msdi.Core/CrossCuttingConcerns/Caching/Redis/RedisCacheManager.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Msdi.Core.CrossCuttingConcerns.Caching.Redis
 {
-    class RedisCacheManager
+    public class RedisCacheManager : ICacheManager
     {
+        private readonly IDistributedCache _cache;
+        public RedisCacheManager(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public void Add(string key, object data, int duration = 0)
+        {
+            
+            _cache.Set(key, data as byte[]);
+        }
+
+        public T Get<T>(string key)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (var memoryStream = new MemoryStream())
+            {
+                var bytes = _cache.Get(key);
+                if (bytes is null) return default(T);
+
+                memoryStream.Write(bytes, 0, bytes.Length);
+
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                return (T)binaryFormatter.Deserialize(memoryStream);
+            }
+        }
+
+        public object Get(string key)
+        {
+            return _cache.Get(key);
+        }
+
+        public bool IsExist(string key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
